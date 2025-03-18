2c8acc4f5458778480c15e785fd8e1b3f81f1927
huynhmanh2003 <136096602+huynhmanh2003@users.noreply.github.com>
2025-03-18 08:59:20 +0700

    fixed api

--- models/board.model.js
+++ models/board.model.js
@@ -5,29 +5,17 @@ const BoardSchema = new mongoose.Schema(
     title: { type: String, required: true },
     description: { type: String, default: null },
     cover: { type: String, default: null },
-    columnId: { type: String, required: false },
-    columnOrderIds: [
+    columnOrderIds: [{ type: String, required: false }],
+    columnIds: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Column",
-      },
-    ],
-    memberIds: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User", // Đảm bảo đây là đúng Model
       },
     ],
     comments: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Comment", // Đảm bảo đây là đúng Model
-      },
-    ],
-    attachments: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Attachment", // Đảm bảo đây là đúng Model
       },
     ],
   },
